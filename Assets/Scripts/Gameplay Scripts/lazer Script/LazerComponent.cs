0b1cdc70aa574a2b7262309afcbfe98911bc122c
Olivier Castonguay <202128782@edu.clg.qc.ca>
2023-02-22 21:20:05 -0500

    more Schlick

--- Assets/Scripts/Gameplay Scripts/lazer Script/LazerComponent.cs
+++ Assets/Scripts/Gameplay Scripts/lazer Script/LazerComponent.cs
@@ -25,20 +25,27 @@ public class LazerComponent : MonoBehaviour
    {
        if (collision.contacts[0].otherCollider.gameObject.layer == 6)
        {
-           n2 = collision.contacts[0].otherCollider.GetComponent<MeshRenderer>().materials[0].GetFloat("_Glossiness");
+           n2 = 1- collision.contacts[0].otherCollider.GetComponent<MeshRenderer>().materials[0].GetFloat("_Glossiness");
            angelInDeg = (Vector3.Angle(rig.velocity, collision.contacts[0].normal));
            angelInRad = Mathf.Deg2Rad * angelInDeg;
            damage *= Schlick(n1, n2, angelInDeg);
-           Debug.Log(Schlick(n1, n2, angelInRad));
+           Debug.Log(damage);
+           //  Debug.Log(Schlick(n1, n2, angelInRad));
        }
 
    } 
    public static float Schlick(float n1, float n2, float angle)
    {
             var cosTheta = -Mathf.Cos(angle*Mathf.Deg2Rad);
+            //Debug.Log("cost"+cosTheta);
            float r0 = Mathf.Pow((n1 - n2) / (n1 + n2),2);
-           
+          // Debug.Log("r0"+r0);
+          
            float x = 1 - cosTheta;
-           return r0 + (1 - r0) * x * x * x * x * x;
+          // Debug.Log("x"+x);
+          
+           float val = r0 + (1 - r0) * Mathf.Pow(x,5);
+           
+           return Mathf.Clamp(val*2,0,1) ;
        }
 }
