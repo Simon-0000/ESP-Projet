347bbb46e06976f8c2dcc9cbd1723e24a810ff19
Olivier Castonguay <202128782@edu.clg.qc.ca>
2023-02-24 10:47:35 -0500

    test

--- Assets/Scripts/Gameplay Scripts/lazer Script/LazerComponent.cs
+++ Assets/Scripts/Gameplay Scripts/lazer Script/LazerComponent.cs
@@ -27,22 +27,25 @@ public class LazerComponent : MonoBehaviour
        {
            n2 = 1- collision.contacts[0].otherCollider.GetComponent<MeshRenderer>().materials[0].GetFloat("_Glossiness");
            angelInDeg = (Vector3.Angle(rig.velocity, collision.contacts[0].normal));
-           angelInRad = Mathf.Deg2Rad * angelInDeg;
            damage *= Schlick(n1, n2, angelInDeg);
            Debug.Log(damage);
            //  Debug.Log(Schlick(n1, n2, angelInRad));
+       }
+
+       if (collision.contacts[0].otherCollider.gameObject.layer == 7)
+       {
+           // var Zombie = GetComponent<ZombieBehavior>();
+           //Zombie.takeDamage(damage);
+           //destroy(gameObject);
        }
 
    } 
    public static float Schlick(float n1, float n2, float angle)
    {
             var cosTheta = -Mathf.Cos(angle*Mathf.Deg2Rad);
-            //Debug.Log("cost"+cosTheta);
            float r0 = Mathf.Pow((n1 - n2) / (n1 + n2),2);
-          // Debug.Log("r0"+r0);
           
            float x = 1 - cosTheta;
-          // Debug.Log("x"+x);
           
            float val = r0 + (1 - r0) * Mathf.Pow(x,5);
            
