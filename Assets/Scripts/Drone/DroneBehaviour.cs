57eb6b65e6aec7a7ac1ff124c05980ddad8e8a78
Michael Bruneau <202163092@edu.clg.qc.ca>
2023-03-29 17:19:49 -0400

    DroneBehaviour est complété pour le trajectoire du drone pour suivre le joueur ainsi que vous son déplacement dans l'axe des Y. Avancement du code permettant de suivre un zombie.

--- Assets/Scripts/Drone/DroneBehaviour.cs
+++ Assets/Scripts/Drone/DroneBehaviour.cs
@@ -1,51 +1,120 @@
+using System;
+using Unity;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using static iTween;
+using Assets;
+
+
+using Pixelplacement;
+using Unity.VisualScripting;
+using UnityEditor.TextCore.Text;
+using Random = UnityEngine.Random;
 
 public class DroneBehaviour : MonoBehaviour {
 
-    public Transform target; // the player to follow
-    public float speed = 3.0f; // the speed of the drone
-    public float followDistance = 2.0f; // the distance to maintain from the player
-    public float splineTime = 5.0f; // the time for the drone to follow the spline
+    public Transform target; // le joueur à suivre
+    public Transform target2; // le zombie le plus près
+    public float speed = 1.0f; // la vitesse du drone
+    public float followDistance = 6.0f; // la distance à laquelle se tenir par rapport au joueur
+    public float maximumDistance = 7.0f;
+    private Vector3 lastPt = Vector3.zero;
+    private Vector3 lastTargetPos;
+    public VectorRange possibleSplineSize;
+    private Queue<Vector3> splinePoints = new(); // les points pour la spline
+    private int splineDirection = 1; // -1 à droite, 1 à gauche
+    const int KBN = 5; // multiplicateur des vecteurs bi-normaux
+    public float ptParMetre = 0.2f;
+    private Hashtable args;
+    private bool restartSplines = true;
+    private Vector3 lastPLayerPoint;
+    private Vector3 vecteurY;
+    private int rnd;
 
-    private Vector3[] splinePoints; // the points for the spline
-
-    void Start () {
-        MakePath();
-    }
-
-    void MakePath()
+    void Awake()
     {
-        // create the spline points
-        List<Vector3> points = new List<Vector3>();
-        points.Add(transform.position);
-        for (int i = 0; i < 5; i++) {
-            Vector3 point = new Vector3(Random.Range(transform.position.x, target.transform.position.x), Random.Range(transform.position.y, target.transform.position.y), Random.Range(transform.position.z, target.transform.position.z));
-            points.Add(point);
-        }
-        points.Add(target.position);
-        splinePoints = points.ToArray();
-
-        // set up the iTween path
-        Hashtable args = new Hashtable();
-        args.Add("path", splinePoints);
-        args.Add("time", splineTime);
-        args.Add("easetype", iTween.EaseType.easeInOutSine);
-        //args.Add("looptype", iTween.LoopType.loop);
-        iTween.MoveTo(gameObject,args);
+        Debug.Assert(maximumDistance > followDistance);
+    }
+    void Start () {
+        MakePath(target);
     }
 
-    void Update () {
-        // move the drone towards the player
-        //Vector3 targetPosition = target.position - target.forward * followDistance;
-        //transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * speed);
-
-        // rotate the drone towards the player
-        //Vector3 targetRotation = Quaternion.LookRotation(target.position - transform.position).eulerAngles;
-        //transform.rotation = Quaternion.Euler(0, targetRotation.y, 0);
+    void MakePath(Transform target)
+    {
+        // définir la hauteur du drone en Y
+        rnd = Random.Range(-2, 10);
         
+        // Création des points pour la Spline
+        Vector3 newPosition = transform.position;
+        Vector3 distanceObjet = -(newPosition - (target.transform.position - (target.transform.position - newPosition).normalized * followDistance));
+        if (distanceObjet.magnitude / 2 < ptParMetre)
+        {
+            splinePoints.Clear();
+            return;
+        }
+            
+        int nbrPoints = ((int) (distanceObjet.magnitude / Mathf.Min(ptParMetre, distanceObjet.magnitude / 2 - GameConstants.ACCEPTABLE_ZERO_VALUE)) / 2) * 2;
+        distanceObjet /= nbrPoints;
+        Vector3 binormal = Vector3.Cross(distanceObjet, transform.up).normalized * KBN;
+        List<Vector3> points = new List<Vector3>();
+        for (int i = 0; i < nbrPoints; i++)
+        {
+            splineDirection *= -1;
+            newPosition += distanceObjet + splineDirection * binormal;
+            newPosition = new Vector3(newPosition.x, rnd, newPosition.z);
+            points.Add(newPosition);
+        }
+
+        lastPLayerPoint = points[points.Count - 1];
+        float time = speed;//(distanceObjet.magnitude * nbrPoints) / speed;
+        
+        splinePoints =new Queue<Vector3>(points);
+        
+        // Instanciation du trajet iTween
+        args = new Hashtable();
+        //args.Add("path", splinePoints);
+        args.Add("time", time);
+        args.Add("oncomplete", "MoveToNextPoints" );
+
+        args.Add("easetype", iTween.EaseType.easeInSine);
+        if (restartSplines)
+        {
+            restartSplines = false;
+            MoveToNextPoints();
+        }
+        //iTween.MoveTo(gameObject,args);
+    }
+
+    void MoveToNextPoints()
+    {
+        if (splinePoints.Count == 0)
+        {
+            restartSplines = true;
+            Debug.Log("no more points");
+            return;
+            
+        }
+        Vector3[] nextPoints = new[] { splinePoints.Dequeue(), splinePoints.Dequeue() };
+        args.Remove("path");
+        args.Add("path", nextPoints);
+        iTween.MoveTo(gameObject,args);
+        lastPt = nextPoints[nextPoints.Length - 1];
+
+
+    }
+
+    void Update ()
+    {
+        if (//Algos.GetVectorAbs(transform.position - lastPt).magnitude <= GameConstants.OVERLAP_TOLERANCE &&
+            Algos.GetVectorAbs(target.transform.position - lastPLayerPoint).magnitude > maximumDistance)
+            
+        {
+            if (Algos.GetVectorAbs(transform.position - target.position).magnitude > maximumDistance)
+                MakePath(target);
+            else
+                splinePoints.Clear();
+        }
+                
         
         
     }
